222477931e1f1702cb37ea68489fca75281c1074
nikolay.minyakin <nikolay.minyakin@x5.ru>
2023-12-07 20:01:17 +0300

    stable_version_1_ver_2

--- v1/models.py
+++ v1/models.py
@@ -14,3 +14,14 @@ class Genre(models.Model):
 
     class Meta:
         ordering = ["slug"]
+
+
+class Conflict(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
+
+    def __str__(self):
+        return self.slug
+
+    class Meta:
+        ordering = ["slug"]
